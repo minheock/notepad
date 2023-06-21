e4293e33c21835450817230cdabea22edaa4107e
minheock <dygks0602@naver.com>
2023-06-21 11:58:13 +0900

    새파일 , 저장하기, 종료하기 기능 구현

--- main.py
+++ main.py
@@ -1,24 +1,45 @@
 from tkinter import *
 from tkinter.filedialog import *
 
+# 기본뼈대
 window = Tk()
 window.title("메모장")
 window.geometry("400x400")
 window.resizable(False, False)
 
+# 동작추가
+def new_file() :
+    text_area.delete(1.0, END)
+
+def save_file() :
+    f = asksaveasfile(mode="W", defaultextension=".txt", filetypes=[('Text files', '.txt')])
+    text_save = str(text_area.get(1.0, END))
+    f.write(text_save)
+    f.close()
+def maker() :
+    help_view = Toplevel(window)
+    help_view.geometry("300x50")
+    help_view.title("Maker")
+    lb = Label(help_view, text='minheock이 만든 메모장입니다.')
+    lb.pack()
+
+
+
+# 메뉴
 menu = Menu(window)
 menu_1 = Menu(menu, tearoff=0)
-menu_1.add_command(label='새파일')
+menu_1.add_command(label='새파일', command=new_file)
 menu_1.add_separator()
-menu_1.add_command(label='저장하기')
+menu_1.add_command(label='저장하기', command=save_file)
 menu_1.add_separator()
 menu_1.add_command(label='종료', command=window.destroy)
 menu.add_cascade(label='파일', menu=menu_1)
 
 menu_2 = Menu(menu, tearoff=0)
-menu_2.add_command(label='min')
+menu_2.add_command(label='min', command=maker)
 menu.add_cascade(label='만든이', menu=menu_2)
 
+# 텍스트
 text_area = Text(window)
 window.grid_rowconfigure(0, weight=1)
 window.grid_columnconfigure(0, weight=1)
